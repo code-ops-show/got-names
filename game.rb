4cb60b39b8d341eff82bb8bfe3b7aa76b8103949
Zack Siri <zack@codemy.net>
2014-06-22 16:58:25 +0700

    added game.rb

---
+++ game.rb
@@ -0,0 +1,7 @@
+name = "Zack Siri"
+
+first_letter_first_name = name.split(' ').first[0]
+last_letter_last_name   = name.split(' ').last[0]
+
+puts first_letter_first_name
+puts last_letter_last_name
