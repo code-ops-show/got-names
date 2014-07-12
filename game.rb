345ed89c21ffb65b63d6a5723afc68c53be1ac3c
Zack Siri <zack@codemy.net>
2014-07-12 22:03:49 +0700

    setup yaml file

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 name = "Zack Siri"
 
 # first_letter_first_name = name.split(' ').first[0]
@@ -8,9 +10,11 @@ first_initial, last_initial = name.split(' ').map { |n| n[0] }
 puts first_initial
 puts last_initial
 
-mens_first_name = { a: "Davos", b: "Viserys", c: "Brandon" }
-house_name  = { a: "Dondarrion", b: "Clegane", c: "Greyjoy" }
+got_data = YAML.load(File.open(File.join(File.dirname(__FILE__), 'got_names.yaml')))
 
-"#{mens_first_name[:a]} of house #{house_name[:b]}"
+mens_first_name = got_data[:mens_first_name]
+house_name = got_data[:house_name]
+
+puts "#{mens_first_name[first_initial.downcase.to_sym]} of house #{house_name[last_initial.downcase.to_sym]}"
 
 
